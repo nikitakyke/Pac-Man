3b3df01622a19b8b97569857009dc2fe5e8d1072
nikitakyke <55665235+nikitakyke@users.noreply.github.com>
2019-12-08 01:53:41 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,10 +1,42 @@
 from tkinter import *
-
+B = []
+class food():
+		def __init__(self, x, y):
+			self.x = x
+			self.y = y
+			self.r = 7
+			self.color='yellow'
+			self.id = canv.create_oval(
+                -20+40*self.x - self.r,
+                -20+40*self.y - self.r,
+                -20+40*self.x + self.r,
+                -20+40*self.y + self.r,
+                fill=self.color
+				)
+		def set_coords(self):
+			"""Параметры рисования еды"""
+			canv.coords(
+                self.id,
+                -20+40*self.x - self.r,
+                -20+40*self.y - self.r,
+                -20+40*self.x + self.r,
+                -20+40*self.y + self.r
+				)
+		def delete(self):
+			self.r = 0
+			self.set_coords()
 def create_lab():
+	global B
 	"""Создание экрана и лабиринта"""
 	#Лабиринт
 	canv.create_rectangle(0, 0, 760, 280, fill='black', outline='green',
                           width=0)
+                         
+	for i in range(1, 20, 1):
+		for j in range(1, 8, 1):
+			new_food = food(i, j)
+			B += [(i, j, new_food)]
+				
 	#Буква M
 	canv.create_rectangle(40, 40, 80, 240, fill='blue', outline='green',
                           width=0)
