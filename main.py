6c75dead6b98fc6d6a3d5dcd41e8aaa1d41e7fb9
Andrew Kotsevich <55478660+Andrew-Kot@users.noreply.github.com>
2019-12-11 09:05:09 +0300

    Главный модуль игры

--- main.py
+++ main.py
@@ -1,62 +1,44 @@
 from tkinter import *
-#import datetime
 import graph as gr
 import objects as obj
 import time
-def main():
+
+root = Tk()
+root.geometry('760x380')
+root.title("Pac - Man.Version: MIPT")
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=10)
+#массив привидений
+ghosts = []
+#количество привидений
+N_ghost = 1
+
+#Вывод очков
+label = Label(bg='white', fg='black', width=20)
+score = 0
+label['text'] = score
+label.pack()
+live = 0
+def new_game():
+	global live, ghosts, pacman
 	# Главная функция игры
-	root = Tk()
-	root.geometry('760x380')
-	root.title("Pac - Man.Version: MIPT")
-	canv = Canvas(root, bg='white')
-	canv.pack(fill=BOTH, expand=10)
-	
-	print("Игра началась!")
-	"""time_0 = datetime.datetime.now()"""
-	
 	# Создание лабиринта
 	gr.canv = canv
 	gr.create_lab()
-	"""создание шарика"""
 	obj.canv = canv
-	#new_ball=b.Ball()
+	print("Игра началась!")
 	"""создание пакмана"""
 	pacman = obj.Pacman()
-	"""создание привидений"""
+	live = 3
 	ghosts = []
-	for i in range (1):
-		new_ghost=obj.Ghost()
+	"""создание привидений"""
+	for i in range (N_ghost):
+		new_ghost=obj.Ghost(7 - (i%6))
 		ghosts += [new_ghost]
 	# Нижняя панель с текущим временем игры и кнопками
 	frame = Frame(root)
 	frame.pack(side=BOTTOM)
-
-	"""# Вывод времени на экран
-	time = datetime.datetime.now()
-	delta_time = time - time_0
-	displayed_time = StringVar()
-	displayed_time.set("Время игры: " + str(delta_time))
-	time_label = Label(frame, textvariable=displayed_time, width=30)
-	time_label.pack(side=LEFT)
-"""
-	# Кнопка "Up"
-	"""up_button = Button(frame, text=" Up ", height=2,
-							command=new_ball.Button_up)
-	up_button.pack(side=TOP)
-	# Кнопка "Down"
-	down_button = Button(frame, text="Down", height=2, 
-						command=new_ball.Button_down)
-	down_button.pack(side=BOTTOM)
-	#Кнопка "Left"
-	left_button = Button(frame, text="Left", width=6,
-							command=new_ball.Button_left)
-	left_button.pack(side=LEFT)
-	# Кнопка "Right"
-	right_button = Button(frame, text="Right", width=6, 
-						command=new_ball.Button_right)
-	right_button.pack(side=RIGHT)"""
-	
-	# Управление кнопками с клавиатуры
+	#Управление кнопками с клавиатуры
 	#Движение вверх
 	root.bind("w", pacman.Button_up)
 	root.bind("<Up>", pacman.Button_up)
@@ -69,15 +51,33 @@ def main():
 	#Движение вправо
 	root.bind("d", pacman.Button_right)
 	root.bind("<Right>", pacman.Button_right)
-	
-	#Вывод очков
-	score = 0
-	label = Label(bg='white', fg='black', width=20)
-	label['text'] = score
-	label.pack()
-	#obj.Ghost.angle_x(pacman)
-	mainloop()
 
+# Вывод времени на экран
+def game():
+	global live
+	for i in range(0,N_ghost, 1):
+		ghosts[i].ghost_move(pacman)
+		if ghosts[i].distance<0.01:
+			live-=1
+			screen = canv.create_text(380, 300, text = 'Game over!', font = '28')
+			canv.update()
+			time.sleep(3)
+			new_game()
+			game()
+		#счет очков и вывод на экран
+		score = pacman.record
+		label['text'] = score
+		if pacman.record == 50:
+			screen = canv.create_text(380, 300, text = 'Victory!', font = '28')
+			canv.update()
+			time.sleep(3)
+			new_game()
+			game()
+	root.after(300, game)
+new_game()
+game()
+
+mainloop()
 
 if __name__ == "__main__":
-    main()
+	new_game()
